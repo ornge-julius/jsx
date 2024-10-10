1fb44f68c74365583c9481654b843c07584be566
Julius Ware <jcware@covermymeds.com>
2024-10-10 10:23:54 -0400

    Add App component and use

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-// 1) Import the React and ReactDOM libraries
+// 1) Import the React and ReactDOM libraries and App component
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from "./App";
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById('root');
@@ -8,10 +9,5 @@ const el = document.getElementById('root');
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
-// 4) Create a component
-function App() {
-	return <textarea spellCheck={true} />;
-}
-
-// 5) Show the component on the screen
+// 4) Show the component on the screen
 root.render(<App />)
